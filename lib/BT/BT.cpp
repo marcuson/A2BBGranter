582defc8cee8b6f674b593fb483f20aacafb5324
Leonardo Marcuson <marcuson.nn90@gmail.com>
2017-02-24 18:16:06 +0100

    Add class to deal with bluetooth.

--- lib/BT/BT.cpp
+++ lib/BT/BT.cpp
@@ -6,40 +6,204 @@ char BT::PIN[] = "1234";
 char BT::NAME[] = "HC-05";
 char BT::MAC[] = "20:16:11:28:31:26";
 
-BT::BT(HardwareSerial &serial, int enPin, int statePin)
+BT::BT(HardwareSerial &serial, int enPin)
 : _serial(serial)
 {
   _qrInfo = new char[strlen(MAC) + strlen(NAME) + 1];
-  Serial.println("1");
   strcpy(_qrInfo, MAC);
   strcat(_qrInfo, ",");
   strcat(_qrInfo, NAME);
-  Serial.println("2");
 
-  _serial.begin(BAUD);
   _enPin = enPin;
-  _statePin = statePin;
 }
 
-BT::~BT() {
+BT::~BT()
+{
   delete [] _qrInfo;
 }
 
-void BT::init() {
+void BT::init()
+{
   pinMode(_enPin, OUTPUT);
-  pinMode(_statePin, INPUT);
+
   setEnabled(true);
+
+  _serial.begin(BAUD);
 }
 
-void BT::setEnabled(bool value) {
+bool BT::isEnabled()
+{
+  return digitalRead(_enPin) == HIGH;
+}
+
+void BT::setEnabled(bool value)
+{
   digitalWrite(_enPin, value ? HIGH : LOW);
-  _isEnabled = value;
 }
 
-char *BT::pin() {
+char *BT::pin()
+{
   return PIN;
 }
 
-char *BT::infoForQR() {
+char *BT::infoForQR()
+{
   return _qrInfo;
+}
+
+int BT::available()
+{
+  return _serial.available();
+}
+
+int BT::readAsPossible()
+{
+  int toRead = available();
+  int actuallyRead = 0;
+
+  if (toRead > 0)
+  {
+    for (int i = 0; i < toRead; i++)
+    {
+      if (_readBufferIndex >= _readBufferSize - 1)
+      {
+        break;
+      }
+
+      char c = (char) _serial.read();
+
+      Serial.print("Arrived: ");
+      Serial.println(c);
+
+      _readBuffer[_readBufferIndex] = c;
+      _readBuffer[_readBufferIndex + 1] = '\0';
+      _readBufferIndex++;
+
+      if (c == '+')
+      {
+        _plusFound++;
+      }
+    }
+
+    if (_readBuffer[0] != '+')
+    {
+      reset();
+    }
+  }
+
+  return actuallyRead;
+}
+
+bool BT::hasCommandInBuffer()
+{
+  if (_readBufferIndex < 6 || _plusFound < 3)
+  {
+    return false;
+  }
+
+  return _readBuffer[0] == '+' && _readBuffer[4] == '+' && _readBuffer[_readBufferIndex - 1] == '+';
+}
+
+void BT::executeCommand(char otp[], LCD &lcd)
+{
+  if (!hasCommandInBuffer())
+  {
+    return;
+  }
+
+  lcd.clear();
+
+  Serial.print("From BT received: ");
+  Serial.println(_readBuffer);
+
+  char comm[4];
+  strncpy(comm, _readBuffer + 1, 3);
+  comm[3] = '\0';
+
+  if (strcmp(comm, "dev") == 0)
+  {
+    char *commaPtr = strchr(_readBuffer + 4, ',');
+    int commaPos = commaPtr - _readBuffer;
+
+    int devIdSize = commaPos - 5;
+    char devId[devIdSize + 1];
+    strncpy(devId, _readBuffer + 5, devIdSize);
+    devId[devIdSize] = '\0';
+
+    int otpSize = _readBufferIndex - 1 - commaPos - 1;
+    if (!_isFirstCheck)
+    {
+      otpSize -= 2; // Hack because 2nd call to qrduino library messes up BT serial communication
+    }
+    char checkOtp[otpSize + 1];
+    strncpy(checkOtp, commaPtr + 1, otpSize);
+    checkOtp[otpSize] = '\0';
+
+    _isFirstCheck = false;
+
+    Serial.print("Try to grant device with id: ");
+    Serial.println(devId);
+
+    Serial.print("OTP provided: '");
+    Serial.print(checkOtp);
+
+    if (strcmp(checkOtp, otp) != 0)
+    {
+      Serial.print("' is NOT valid (needed '");
+      Serial.print(otp);
+      Serial.println("')");
+
+      send("nack;");
+
+      lcd.drawFail();
+
+      reset();
+      return;
+    }
+
+    Serial.println("' is valid");
+
+    lcd.drawText("Granting access to device: ", 0, 0, true);
+    lcd.drawText(devId);
+    lcd.drawText("\n");
+
+    // FIXME Do REST call
+
+    Serial.println("OK! Sending ack back");
+    lcd.drawText("OK! Sending ack back");
+
+    send("ack;");
+
+    lcd.drawSuccess();
+  }
+
+  reset();
+}
+
+void BT::reset()
+{
+  _plusFound = 0;
+  _readBufferIndex = 0;
+}
+
+void BT::printDebug()
+{
+  Serial.print("BT buffer: ");
+
+  for (int i = 0; i < 101; i++)
+  {
+    if (_readBuffer[i] == '\0')
+    {
+      break;
+    }
+
+    Serial.print(_readBuffer[i]);
+  }
+
+  Serial.println();
+}
+
+void BT::send(char *data)
+{
+  _serial.print(data);
 }
--- lib/BT/BT.hpp
+++ lib/BT/BT.hpp
@@ -2,15 +2,19 @@
 #define __BT_H__INCLUDE__
 
 #include "Arduino.h"
+#include <LCD.hpp>
 
 class BT
 {
 private:
-  bool _isEnabled;
   HardwareSerial &_serial;
   int _enPin;
-  int _statePin;
   char *_qrInfo;
+  int _readBufferIndex = 0;
+  char _readBuffer[101];
+  int _readBufferSize = sizeof(_readBuffer) / sizeof(_readBuffer[0]);
+  int _plusFound = 0;
+  bool _isFirstCheck = true;
 
 public:
   static int BAUD;
@@ -22,12 +26,20 @@ public:
   static char NAME[]; // See comment on PIN
   static char MAC[]; // See comment on PIN
 
-  BT(HardwareSerial &serial, int enPin, int statePin);
+  BT(HardwareSerial &serial, int enPin);
   ~BT();
   void init();
+  bool isEnabled();
   void setEnabled(bool value);
   char *infoForQR();
   char *pin();
+  int available();
+  int readAsPossible();
+  bool hasCommandInBuffer();
+  void executeCommand(char *otp, LCD &lcd);
+  void send(char *data);
+  void printDebug();
+  void reset();
 };
 
 #endif
